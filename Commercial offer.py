36ed1e3efea39eb28d81e619821b408bf22a3fed
BobrovKirill <bobrovkirill11@gmail.com>
2019-05-11 16:22:02 +0500

    таблица выровнена по центру и реализовал подсчет итоговой суммы

--- Commercial offer.py
+++ Commercial offer.py
@@ -1,5 +1,7 @@
 from openpyxl import load_workbook
 import docx
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.enum.table import WD_TABLE_ALIGNMENT
 
 
 
@@ -21,17 +23,33 @@ for data in ws.values:
 def request(key):
     if key in all_product:
         i = 1
+        global sum_price
+        sum_price = 0
         for key,val in all_product.items():
             data_cells = table.add_row().cells
             data_cells[0].text = str(i)
+            paragraph = data_cells[0].paragraphs[0]
+            paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
             data_cells[1].text = key
+            paragraph = data_cells[1].paragraphs[0]
+            paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
             data_cells[2].text = str(val[0])
+            paragraph = data_cells[2].paragraphs[0]
+            paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
             data_cells[3].text = str(val[1])
+            paragraph = data_cells[3].paragraphs[0]
+            paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
             data_cells[4].text = str(val[2])
+            paragraph = data_cells[4].paragraphs[0]
+            paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
             data_cells[5].text = str(val[3])
-            i+=1
+            paragraph = data_cells[5].paragraphs[0]
+            paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+            sum_price += val[3]
+            i += 1
     else:
         print('В списке нет данного товара')
+    return sum_price
 
         # Открытие ворда и добавление стилей
 
@@ -53,12 +71,20 @@ for i in range(2):
     merget = table.cell(nums_cells,i).merge(table.cell(nums_cells,i+1))
     merget = table.cell(nums_cells,i+3).merge(table.cell(nums_cells,i+4))
 data_cells[0].text = 'Итого, рублей'
-data_cells[3].text = 'сумма'
+data_cells[3].text = str(sum_price) # !!!! Создать тут формулу суммы
+paragraph = data_cells[0].paragraphs[0]
+paragraph.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+paragraph = data_cells[3].paragraphs[0]
+paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
 data_cells = table.add_row().cells
 for i in range(2):
     merget = table.cell(nums_cells+1,i).merge(table.cell(nums_cells+1,i+1))
     merget = table.cell(nums_cells+1,i+3).merge(table.cell(nums_cells+1,i+4))
 data_cells[0].text = 'В том числе НДС, руб:'
 data_cells[3].text = 'Без НДС*'
+paragraph = data_cells[0].paragraphs[0]
+paragraph.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+paragraph = data_cells[3].paragraphs[0]
+paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
 doc.save(file_save)
